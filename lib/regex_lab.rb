964487109b2cc6534d2c6b79114879940cc7c08e
zachary mines <zmines@wesleyan.edu>
2020-06-30 19:41:17 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -25,8 +25,7 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
-  a = phone.scan(/\S\d/)
-  
+  if phone(/\S\d/) 
     return true 
   else 
     return false 
