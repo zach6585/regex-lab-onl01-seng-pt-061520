ece3c1cbe7b6200e0e59196a92520a475db6bb5d
zachary mines <zmines@wesleyan.edu>
2020-06-30 19:14:57 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,17 +1,23 @@
+require 'pry'
 def starts_with_a_vowel?(word)
-
+  if word.scan(/^[aeiouAEIOU]/) != []
+    return true 
+  else 
+    return false 
+  end 
 end
+# binding.pry
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  text.scan(/un+\w+ing/)
 end
 
 def words_five_letters_long(text)
-
+  text.scan(/\b\w{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+  text.scan(/^[A-Z].[!.?,]\z/)
 end
 
 def valid_phone_number?(phone)
