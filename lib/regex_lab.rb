6d9815771f074e49645160d2073981ff48499020
zachary mines <zmines@wesleyan.edu>
2020-06-30 19:19:02 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -17,7 +17,10 @@ def words_five_letters_long(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-  text.scan(/\A[A-Z].[!.?,]\z/)
+ if text.scan(/\A[A-Z].[!.?,]\z/) != []
+   return true 
+  else 
+    return false 
 end
 
 def valid_phone_number?(phone)
